ed9a231671c43c6289c2bc5971f4e9d177cde35a
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-11-01 04:41:30 -0500

    feat: add correct workspaces lib and make async

--- tModCodeAssist/tModCodeAssist.TypePropagation.Runner/MSBuild/ProjectAnalyzer.cs
+++ tModCodeAssist/tModCodeAssist.TypePropagation.Runner/MSBuild/ProjectAnalyzer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
 
@@ -9,31 +10,11 @@ namespace tModCodeAssist.TypePropagation.Runner.MSBuild;
 /// <summary>
 ///		Analyzes an MSBuild project.
 /// </summary>
-public sealed class ProjectAnalyzer : IDisposable
+public sealed class ProjectAnalyzer(
+	MSBuildWorkspace workspace,
+	Compilation compilation
+) : IDisposable
 {
-	private readonly MSBuildWorkspace workspace;
-	private readonly Compilation compilation;
-
-	public ProjectAnalyzer(string projectPath, string? projectName)
-	{
-		workspace = MSBuildWorkspace.Create();
-
-		Project project;
-		if (projectPath.EndsWith(".sln")) {
-			Solution solution = workspace.OpenSolutionAsync(projectPath).GetAwaiter().GetResult();
-			project = projectName is not null ? solution.Projects.First(x => x.Name == projectName) : solution.Projects.First();
-		}
-		else if (projectPath.EndsWith(".csproj")) {
-			project = workspace.OpenProjectAsync(projectPath).GetAwaiter().GetResult();
-		}
-		else {
-			throw new InvalidOperationException($"Unknown project file kind: {projectPath}");
-		}
-
-		compilation = project.GetCompilationAsync().GetAwaiter().GetResult()
-			?? throw new InvalidOperationException("Failed to get compilation");
-	}
-
 	public SymbolTracker Run()
 	{
 		IEnumerable<(ISymbol symbol, IdKind kind)> seeds = WellKnownSeedProvider.GetSeedsForCompilation(compilation);
@@ -46,5 +27,27 @@ public sealed class ProjectAnalyzer : IDisposable
 	public void Dispose()
 	{
 		workspace.Dispose();
+	}
+
+	public static async Task<ProjectAnalyzer> CreateAsync(string projectPath, string? projectName)
+	{
+		var workspace = MSBuildWorkspace.Create();
+
+		Project project;
+		if (projectPath.EndsWith(".sln")) {
+			Solution solution = await workspace.OpenSolutionAsync(projectPath);
+			project = projectName is not null ? solution.Projects.First(x => x.Name == projectName) : solution.Projects.First();
+		}
+		else if (projectPath.EndsWith(".csproj")) {
+			project = await workspace.OpenProjectAsync(projectPath);
+		}
+		else {
+			throw new InvalidOperationException($"Unknown project file kind: {projectPath}");
+		}
+
+		Compilation compilation = project.GetCompilationAsync().GetAwaiter().GetResult()
+		                       ?? throw new InvalidOperationException("Failed to get compilation");
+
+		return new ProjectAnalyzer(workspace, compilation);
 	}
 }
--- tModCodeAssist/tModCodeAssist.TypePropagation.Runner/Program.cs
+++ tModCodeAssist/tModCodeAssist.TypePropagation.Runner/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
 using tModCodeAssist.TypePropagation.Runner.MSBuild;
 
 namespace tModCodeAssist.TypePropagation.Runner;
 
 internal static class Program
 {
-	public static int Main(string[] args)
+	public async static Task<int> Main(string[] args)
 	{
 		if (args.Length is < 1 or > 2) {
-			Console.Error.WriteLine("Usage: ./propagator.exe <path-to-sln-or-csproj> [project-name]");
+			await Console.Error.WriteLineAsync("Usage: ./propagator.exe <path-to-sln-or-csproj> [project-name]");
 			return 1;
 		}
 
@@ -18,7 +19,7 @@ internal static class Program
 		string projPath = args[0];
 		string? projName = args.Length > 1 ? args[1] : null;
 
-		using var analyzer = new ProjectAnalyzer(projPath, projName);
+		using ProjectAnalyzer analyzer = await ProjectAnalyzer.CreateAsync(projPath, projName);
 		SymbolTracker tracker = analyzer.Run();
 
 		return 0;
